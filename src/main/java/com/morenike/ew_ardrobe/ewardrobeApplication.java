92429184fb9afb27d4e328db6e25a71eded5785e
Morenike Daniel <Morenike.Daniel@Interswitchng.com>
2023-09-21 14:21:57 +0100

    First commit

---
+++ src/main/java/com/morenike/ew_ardrobe/ewardrobeApplication.java
@@ -0,0 +1,13 @@
+package com.morenike.ew_ardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ewardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ewardrobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morenike/ew_ardrobe/EwArdrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morenike.ew_ardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwArdrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
